5c8217b682fa56fdc8fa71d206f274adfb0f070b
osbm <osmanfbayram@gmail.com>
2024-12-10 20:50:35 +0300

    update memory output to display in megabytes

--- main.cu
+++ main.cu
@@ -6,8 +6,8 @@ int main(){
 
     size_t mf, ma;
     cudaMemGetInfo(&mf, &ma);
-    std::cout << "Free memory (gb): " << mf/1024/1024/1024 << std::endl;
-    std::cout << "Total memory (gb): " << ma/1024/1024/1024 << std::endl;
+    std::cout << "Free memory (mb): " << mf/1024/1024 << std::endl;
+    std::cout << "Total memory (mb): " << ma/1024/1024 << std::endl;
 
     return 0;
 }
